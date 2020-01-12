673cafabb09689650ce7d6bd9e20730e7a6ac1df
linying <brucekellan@gmail.com>
2020-01-12 12:06:15 +0800

    Java Stream Demo Commit

--- src/test/java/com/sjb/linying/stream/StreamTest.java
+++ src/test/java/com/sjb/linying/stream/StreamTest.java
@@ -3,23 +3,38 @@ package com.sjb.linying.stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StreamTest {
 
     @Test
-    @DisplayName("Java Collection Stream API 延迟计算")
+    @DisplayName("JavaStream同步API阻塞计算")
     public void runStream() {
-        Arrays.asList(1, 2, 3, 4, 5, 6).stream().map(e -> {
-            System.out.print(e);
+        long current = System.currentTimeMillis();
+        System.out.println("Start 1");
+        Stream.of(1, 2, 3, 4, 5, 6).map(e -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
             return e;
         });
-        System.out.println();
-        Arrays.asList(1, 2, 3, 4, 5, 6).stream().map(e -> {
-            System.out.print(e);
+        System.out.println("End 1");
+        System.out.println("Use " + (System.currentTimeMillis() - current) + "ms");
+        current = System.currentTimeMillis();
+        System.out.println("Start 2");
+        Stream.of(1, 2, 3, 4, 5, 6).map(e -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
             return e;
         }).collect(Collectors.toList());
+        System.out.println("End 2");
+        System.out.println("Use " + (System.currentTimeMillis() - current) + "ms");
     }
 
 }
